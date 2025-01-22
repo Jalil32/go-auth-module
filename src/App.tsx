65bbb2a42689a1d8e72b281f5389610de196cb9f
jayce <jasonrmillman@gmail.com>
2025-01-22 23:38:06 +0800

    add shadcn, tailwind, biome and update homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./index.css";
+import { Button } from "@/components/ui/button";
+import {
+	ChartTooltip,
+	ChartContainer,
+	type ChartConfig,
+	ChartTooltipContent,
+} from "@/components/ui/chart";
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<div className="flex flex-col bg-background max-w-md m-auto gap-y-5">
+				<h1 className="text-3xl font-bold underline">Hello world!</h1>
+				<Button onClick={() => setCount((count) => count + 1)}>Up</Button>
+				<p>{count}</p>
+				<Button onClick={() => setCount((count) => count - 1)}>Down</Button>
+				<ChartComponent />
+			</div>
+		</>
+	);
 }
 
-export default App
+export function ChartComponent() {
+	const chartData = [
+		{ month: "January", desktop: 186, mobile: 80 },
+		{ month: "February", desktop: 305, mobile: 200 },
+		{ month: "March", desktop: 237, mobile: 120 },
+		{ month: "April", desktop: 73, mobile: 190 },
+		{ month: "May", desktop: 209, mobile: 130 },
+		{ month: "June", desktop: 214, mobile: 140 },
+	];
+
+	const chartConfig = {
+		desktop: {
+			label: "Desktop",
+			color: "#2563eb",
+		},
+		mobile: {
+			label: "Mobile",
+			color: "#60a5fa",
+		},
+	} satisfies ChartConfig;
+
+	return (
+		<ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+			<BarChart accessibilityLayer data={chartData}>
+				<CartesianGrid vertical={false} />
+				<XAxis
+					dataKey="month"
+					tickLine={false}
+					tickMargin={10}
+					axisLine={false}
+					tickFormatter={(value) => value.slice(0, 3)}
+				/>
+				<ChartTooltip content={<ChartTooltipContent />} />
+				<Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+				<Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+			</BarChart>
+		</ChartContainer>
+	);
+}
+
+export default App;
