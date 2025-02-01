13965f10d11aaeb45b39487c7be159370011aad0
LloydN01 <lloydna0527@gmail.com>
2025-02-01 22:44:15 +0800

    add foreign key constraints

--- backend/migrations/20250201072343_create_bank_transactions_table.sql
+++ backend/migrations/20250201072343_create_bank_transactions_table.sql
@@ -5,7 +5,11 @@ CREATE TABLE IF NOT EXISTS bank_transactions (
     user_id INT NOT NULL,						-- Foreign key to the user who made the transaction
     date TIMESTAMP NOT NULL,				-- Date of transaction
     amount_cents INT NOT NULL,					-- Transaction amount in cents
-    description TEXT NOT NULL						-- Transaction description: Default is "No description"
+    description TEXT NOT NULL,						-- Transaction description: Default is "No description"
+    CONSTRAINT fk_user_id 
+        FOREIGN KEY (user_id) 
+        REFERENCES users(id) 
+        ON DELETE CASCADE -- Foreign key constraint: When a user is deleted, delete all their transactions
 );
 -- +goose StatementEnd
 
