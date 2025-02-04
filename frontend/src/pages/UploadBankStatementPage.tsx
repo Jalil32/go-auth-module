8ac728c5cbbb9e1e80f7e5b6bb405a919f0880a9
LloydN01 <lloydna0527@gmail.com>
2025-02-04 15:01:27 +0800

    add file input component

--- frontend/src/pages/UploadBankStatementPage.tsx
+++ frontend/src/pages/UploadBankStatementPage.tsx
@@ -1,7 +1,20 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import GenericPageTemplate from "./GenericPageTemplate";
 
 const UploadBankStatementPage = () => {
-	return <GenericPageTemplate pageContent="Upload Bank Statement" />;
+	const bankStatementUpload = (
+		<div className="flex flex-1 flex-col p-4 pt-0">
+			<div className="flex-1 rounded-xl bg-muted/50">
+				<div className="">
+					<Label htmlFor="picture">Picture</Label>
+					<Input id="picture" type="file" />
+				</div>
+			</div>
+		</div>
+	);
+
+	return <GenericPageTemplate pageContent={bankStatementUpload} />;
 };
 
 export default UploadBankStatementPage;
