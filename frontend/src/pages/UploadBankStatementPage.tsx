1f43bf6ccd163e5db0cdb677f16496db2cd074d4
LloydN01 <lloydna0527@gmail.com>
2025-02-04 17:36:54 +0800

    add header and subheader for upload files

--- frontend/src/pages/UploadBankStatementPage.tsx
+++ frontend/src/pages/UploadBankStatementPage.tsx
@@ -1,7 +1,7 @@
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
+import { Upload } from "lucide-react";
 import Papa from "papaparse";
 import { useState } from "react";
 import GenericPageTemplate from "./GenericPageTemplate";
@@ -26,26 +26,39 @@ const UploadBankStatementPage = () => {
 	const bankStatementUpload = (
 		<div className="flex flex-1 p-4 pt-0">
 			<div className="flex flex-col md:flex-row flex-1 rounded-xl bg-muted/50 space-y-4 md:space-x-4">
-				<div className="flex-1">
-					<Label htmlFor="csv">Upload your bank statement</Label>
-					<Input
-						id="csv"
-						type="file"
-						accept=".csv"
-						onChange={uploadHandler}
-					/>
+				<div className="flex flex-col flex-1 space-y-10 m-5">
+					<div className="space-y-2">
+						<h1 className="flex flex-row items-center text-2xl font-semibold">
+							<Upload className="mr-2" /> Upload File
+						</h1>
+						<p>
+							Upload your bank statement here to record your
+							transactions
+						</p>
+					</div>
+					<div>
+						<Input
+							id="csv"
+							type="file"
+							accept=".csv"
+							onChange={uploadHandler}
+						/>
+						<p className="text-xs text-center">
+							Supported file types: .csv
+						</p>
+					</div>
 				</div>
 				<div className="flex items-center justify-center">
 					<Separator
-						className="hidden md:block h-[90%] border-muted/50"
+						className="hidden md:block h-[95%] border-muted/50"
 						orientation="vertical"
 					/>
 					<Separator
-						className="block md:hidden w-[90%] border-muted/50"
+						className="block md:hidden w-[95%] border-muted/50"
 						orientation="horizontal"
 					/>
 				</div>
-				<div className="flex-1 overflow-auto">
+				<div className="flex-1 m-5 overflow-auto">
 					<Table>
 						<TableBody>
 							{statementData.map(
