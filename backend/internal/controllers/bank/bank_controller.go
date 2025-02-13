2e5ab394c471e5bae66045192201c1cdced7c8f2
LloydN01 <lloydna0527@gmail.com>
2025-02-13 20:49:16 +0800

    changes to db error control

--- backend/internal/controllers/bank/bank_controller.go
+++ backend/internal/controllers/bank/bank_controller.go
@@ -1,7 +1,6 @@
 package bank
 
 import (
-	"encoding/csv"
 	"log/slog"
 	"net/http"
 	"regexp"
@@ -26,94 +25,101 @@ func NewBankController(logger *slog.Logger, db *sqlx.DB) *BankController {
 }
 
 func (bc *BankController) UploadBankStatement(c *gin.Context) {
-	file, err := c.FormFile("file") // Single File Upload
-	if err != nil {
-		bc.Logger.Error("Failed to retrieve file from request", "error", err)
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to retrieve file"})
+	var records [][]interface{} // two-dimensional slice to represent transaction history table
+
+	// Bind the JSON data from the request body to the records interface
+	if err := c.ShouldBindJSON(&records); err != nil {
+		bc.Logger.Error("Failed to parse JSON", "error", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to parse JSON"})
 		return
 	}
 
-	// Open the uploaded file
-	f, err := file.Open()
-	if err != nil {
-		bc.Logger.Error("Failed to open file", "error", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to open file"})
-		return
-	}
-	defer f.Close() // Guarantee the file is closed
-
-	// Parse the CSV file
-	reader := csv.NewReader(f)
-	records, err := reader.ReadAll()
-	if err != nil {
-		bc.Logger.Error("Failed to parse CSV file", "error", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse CSV file"})
-		return
-	}
-
-	// Regex pattern to match date, amount and description values
-	datePattern := regexp.MustCompile(`^\d{2}/\d{2}/\d{4}$`)
-	amountPattern := regexp.MustCompile(`^-?\d+\.\d{2}$`)
+	// Regex pattern to validate date and amount values
+	datePattern := regexp.MustCompile(`^\d{1,2}/\d{1,2}/\d{4}$`) // e.g. "11/11/2011"
+	amountPattern := regexp.MustCompile(`^-?\d+(\.\d{1,2})?$`)   // e.g. "-23.50", "11.40", or "100"
 
 	// Create a slice to store the transactions
 	var transactions []models.Transaction
 
-	// Iterate over the records and store them in the map
-	for _, record := range records {
-		var date time.Time
-		var amount int                            // Amount in cents
-		var description string = "No description" // Default description
+	// Grab the index of each header to identify which column is date, amount and description
+	header := records[0]
+	dateIndex, amountIndex, descriptionIndex := -1, -1, -1
+	for i, h := range header {
+		switch h {
+		case "Date":
+			dateIndex = i
+		case "Amount":
+			amountIndex = i
+		case "Description":
+			descriptionIndex = i
+		}
+	}
 
-		dateFound := false
-		amountFound := false
-		descriptionFound := false
+	if dateIndex == -1 || amountIndex == -1 || descriptionIndex == -1 {
+		bc.Logger.Error("Missing required headers")
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Missing required headers"})
+		return
+	}
 
-		for _, field := range record {
-			if datePattern.MatchString(field) && !dateFound {
-				date, err = time.Parse("02/01/2006", field) // DD/MM/YYYY
-				if err != nil {
-					bc.Logger.Error("Failed to parse date", "error", err)
-					c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse date"})
-					return
-				}
-				dateFound = true
-			} else if amountPattern.MatchString(field) && !amountFound {
-				parsedAmount, err := strconv.ParseFloat(field, 64)
-				if err != nil {
-					bc.Logger.Error("Failed to parse amount", "error", err)
-					c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse amount"})
-					return
-				}
-				amount = int(parsedAmount * 100) // Convert dollars to cents
-				amountFound = true
-			} else if !descriptionFound {
-				description = field
-				descriptionFound = true
-			}
+	// Iterate over the records and store them in the slice
+	for _, record := range records[1:] { // Skip the header row
+
+		// Date data
+		dateStr := record[dateIndex].(string)
+		if !datePattern.MatchString(dateStr) { // User regex to validate date format
+			bc.Logger.Error("Invalid date format", "date", dateStr)
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid date format"})
+			return
+		}
+		date, err := time.Parse("2/1/2006", dateStr) // DD/MM.YYYY
+		if err != nil {
+			bc.Logger.Error("Failed to parse date", "error", err) // User regex to validate amount format
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse date"})
+			return
 		}
 
-		// Create a transaction object if at least the date and amount are found
-		if dateFound && amountFound {
-			// TODO: [WEALT-15] Once Jalil completes authentication, replace the hardcoded User ID with the authenticated User ID
-			transaction := models.Transaction{
-				UserId:      1, // Hardcoded User ID for now
-				Date:        date,
-				AmountCents: amount,
-				Description: description, // Default description is "No description"
-			}
-
-			// Insert the transaction into the database
-			transactionId, err := bc.insertTransaction(transaction)
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to insert transaction"})
-				return
-			}
-			transaction.TransactionId = transactionId
-			transactions = append(transactions, transaction)
-
-			// [Optional] Log the transaction
-			bc.Logger.Info("Transaction inserted successfully", "transaction", transaction)
+		// Amount data
+		amountStr := record[amountIndex].(string)
+		if !amountPattern.MatchString(amountStr) {
+			bc.Logger.Error("Invalid amount format", "amount", amountStr)
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid amount format"})
+			return
 		}
+		amountDollar, err := strconv.ParseFloat(amountStr, 64)
+		if err != nil {
+			bc.Logger.Error("Failed to parse amount", "error", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse amount"})
+			return
+		}
+		amount := int(amountDollar * 100) // Convert dollars to cents
+
+		// Description data
+		description, success := record[descriptionIndex].(string)
+		if !success {
+			// If type assertion fails = record[descriptionIndex] is not a valid string
+			description = "No description" // Default description
+		}
+
+		// TODO: [WEALT-15] Once Jalil completes authentication, replace the hardcoded User ID with the authenticated User ID
+		transaction := models.Transaction{
+			UserId:      1, // Hardcoded User ID for now
+			Date:        date,
+			AmountCents: amount,
+			Description: description, // Default description is "No description"
+		}
+
+		// Insert the transaction into the database
+		transactionId, err := bc.insertTransaction(transaction)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to insert transaction"})
+			return
+		}
+		transaction.TransactionId = transactionId
+		transactions = append(transactions, transaction)
+
+		// [Optional] Log the transaction
+		bc.Logger.Info("Transaction inserted successfully", "transaction", transaction)
+
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "File uploaded successfully", "transactions": transactions})
